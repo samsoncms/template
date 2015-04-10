5288797bf53014ae9fa8392f7014d9178f63fac3
Vitaly Egorov <egorov@samsonos.com>
2015-04-10 17:46:30 +0300

    Going to new design

--- www/menu/new/index.js
+++ www/menu/new/index.js
@@ -2,9 +2,14 @@
  * Created by egorov on 10.04.2015.
  */
 s('.template-menu').pageInit(function(menu){
+
+    var container = s('#template-container');
+
+    // Bind menu collapser
     var collapserBtn = s('.template-menu-collapse', menu).click(function(collapserBtn){
         menu.toggleClass('expanded');
         collapserBtn.toggleClass('fa-times');
         collapserBtn.toggleClass('fa-bars');
+        container.toggleClass('collapsed');
     });
 });
