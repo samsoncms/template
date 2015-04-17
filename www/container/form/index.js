9473828f5d7845909fa02b5c091b162150566108
Vitaly Egorov <egorov@samsonos.com>
2015-04-17 15:38:40 +0300

    Added template-form-input and javascript

---
+++ www/container/form/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by egorov on 17.04.2015.
+ */
+// TODO: Try to remove this when CSS will support it
+s('.template-form-input').pageInit(function(blocks){
+    var inputs = s('input', blocks);
+    // Bind blur event
+    inputs.blur(function(input){
+        var parent = input.parent();
+        // If input is empty - add special class
+        if(input.val().length == 0) {
+            parent.removeClass('filled');
+        } else { // Otherwise remove it
+            parent.addClass('filled');
+        }
+    });
+});
