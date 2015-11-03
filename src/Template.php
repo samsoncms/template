c1a45b23fbd2d682a5677b490ba26ef5ee0dd748
Myslyvyi Dima <expenect@ukr.net>
2015-11-03 16:05:06 +0200

    added to the active menu triangle

--- src/Template.php
+++ src/Template.php
@@ -76,6 +76,7 @@ class Template extends CompressableExternalModule
         $html .= $this->view('menu/item')
                 ->set('icon', 'home')
                 ->set('name', t('Главная', true))
+                ->set('class', 'homeLink')
                 ->set('active', url()->module == '' ? 'active' : '')
                 ->output();
 
